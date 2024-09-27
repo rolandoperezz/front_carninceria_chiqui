5eba200de4c5ccad99c9cb1d2e8eb34d21dcf08a
rolandoperezz <perezqrolando@gmail.com>
2024-09-27 08:42:26 -0600

    cambios

--- src/app/demo/components/auth/principal/principal.component.ts
+++ src/app/demo/components/auth/principal/principal.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validator,FormBuilder,Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [ButtonModule  ],
+  imports: [ButtonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
 export class PrincipalComponent {
+
+
+  constructor(
+    // private ConsultaService : CatalogosService,
+    private ActivatedRoute : ActivatedRoute,
+    private Router : Router,
+    // private formBuilder : FormBuilder,private primengConfig: PrimeNGConfig,
+    // private confirmationService: ConfirmationService, private messageService: MessageService,
+    // private _ExportService: ExportService
+ ) {
+      
+    }
+
+    login(){
+      this.Router.navigateByUrl('/auth/login');
+
+    }
 
 }
